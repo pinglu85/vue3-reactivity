b8f0e9112411434c26f78b981a6bc5805576acfd
pinglu85 <luping8576@hotmail.com>
2021-02-26 21:10:51 +0100

    first commit.

---
+++ README.md
@@ -0,0 +1,3 @@
+### Build a Reactivity System
+
+Following along with the course [Vue 3 Reactivity](https://www.vuemastery.com/courses/vue-3-reactivity/vue3-reactivity/)
---
+++ reactivity.js
@@ -0,0 +1,80 @@
+const targetMap = new WeakMap();
+let activeEffect = null;
+
+function track(target, key) {
+  let depsMap = targetMap.get(target);
+  if (!depsMap) {
+    depsMap = new Map();
+    targetMap.set(target, depsMap);
+  }
+
+  let dep = depsMap.get(key);
+  if (!dep) {
+    dep = new Set();
+    depsMap.set(key, dep);
+  }
+
+  if (activeEffect) {
+    dep.add(activeEffect);
+  }
+}
+
+function trigger(target, key) {
+  const depsMap = targetMap.get(target);
+  if (!depsMap) {
+    return;
+  }
+
+  const dep = depsMap.get(key);
+  if (dep) {
+    dep.forEach((activeEffect) => {
+      activeEffect();
+    });
+  }
+}
+
+function reactive(target) {
+  const handler = {
+    get(target, key, receiver) {
+      const result = Reflect.get(target, key, receiver);
+      track(target, key);
+      return result;
+    },
+    set(target, key, value, receiver) {
+      const oldValue = target[key];
+      const result = Reflect.set(target, key, value, receiver);
+      if (oldValue !== result) {
+        trigger(target, key);
+      }
+      return result;
+    },
+  };
+
+  return new Proxy(target, handler);
+}
+
+function effect(eff) {
+  activeEffect = eff;
+  activeEffect();
+  activeEffect = null;
+}
+
+const product = reactive({ price: 5, quantity: 2 });
+const user = reactive({ firstName: 'John', lastName: 'Doe' });
+
+effect(() => {
+  const total = product.price * product.quantity;
+  console.log(`total = ${total}`);
+});
+
+effect(() => {
+  const userName = `${user.firstName} ${user.lastName}`;
+  console.log(`userName = ${userName}`);
+});
+
+effect(() => {
+  console.log(`Updated quantity to = ${product.quantity}`);
+});
+
+product.price = 10;
+user.firstName = 'Ping';
