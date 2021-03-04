b1dfce336d2f53b4d74d23277102c3b07e732984
pinglu85 <luping8576@hotmail.com>
2021-03-04 11:56:31 +0100

    update documentation comments.

--- 1-building-reacitivity.js
+++ 1-building-reacitivity.js
@@ -1,4 +1,4 @@
-// The main WeakMap that stores {target -> key -> dep} connections.
+// The WeakMap stores {target -> key -> dep} connections.
 const targetMap = new WeakMap();
 let effect = null;
 
