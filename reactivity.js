1cbf7070d102d5695ec54c9e93b89c7ce812a1c8
pinglu85 <luping8576@hotmail.com>
2021-03-05 18:07:36 +0100

    implement ref.

--- reactivity.js
+++ reactivity.js
@@ -1,3 +1,4 @@
+// The WeakMap that stores {target -> key -> dep} connections.
 const targetMap = new WeakMap();
 let activeEffect = null;
 
@@ -8,29 +9,33 @@ function track(target, key) {
 
   let depsMap = targetMap.get(target);
   if (!depsMap) {
-    depsMap = new Map();
-    targetMap.set(target, depsMap);
+    targetMap.set(target, (depsMap = new Map()));
   }
 
   let dep = depsMap.get(key);
   if (!dep) {
-    dep = new Set();
-    depsMap.set(key, dep);
+    depsMap.set(key, (dep = new Set()));
   }
 
   dep.add(activeEffect);
+  console.log('[FUNC TRACK] ACTIVE_EFFECT', activeEffect);
+  console.log('[FUNC TRACK] TARGET', target);
+  console.log('[FUNC TRACK] DEP', dep);
 }
 
 function trigger(target, key) {
+  console.log('[FUNC TRIGGER] TARGET', target);
   const depsMap = targetMap.get(target);
+
   if (!depsMap) {
     return;
   }
 
   const dep = depsMap.get(key);
+  console.log('[FUNC TRIGGER] DEP', dep);
   if (dep) {
-    dep.forEach((activeEffect) => {
-      activeEffect();
+    dep.forEach((innerEffect) => {
+      effect(innerEffect);
     });
   }
 }
@@ -56,16 +61,75 @@ function reactive(target) {
 }
 
 function effect(eff) {
+  console.log('[FUNC EFFECT] ACTIVE_EFFECT', eff);
   activeEffect = eff;
   activeEffect();
   activeEffect = null;
 }
 
+// Make variable reactive
+
+// *** function ref() will cause unexpected behavior when
+// *** the code snippet is executed in online editors like codepen or jsbin.
+// *** 1. The function () => salePrice.value = product.price * 0.9; will be
+// *** added to salePrice dep set.
+// *** 2. console.log(target); in the function track() will cause an infinite loop.
+
+// function ref(rawValue) {
+//   const r = {
+//     get value() {
+//       track(this, 'value');
+//       return rawValue;
+//     },
+//     set value(newValue) {
+//       if (newValue !== rawValue) {
+//         rawValue = newValue;
+//         trigger(this, 'value');
+//       }
+//     },
+//   };
+
+//   return r;
+// }
+
+// Make variable reactive
+class Ref {
+  constructor(rawValue) {
+    this._value = rawValue;
+  }
+
+  get value() {
+    track(this, 'value');
+    return this._value;
+  }
+
+  set value(newVal) {
+    if (newVal !== this._value) {
+      this._value = newVal;
+      trigger(this, 'value');
+    }
+  }
+}
+
+function createRef(rawValue) {
+  return new Ref(rawValue);
+}
+
+function ref(rawValue) {
+  return createRef(rawValue);
+}
+
 const product = reactive({ price: 5, quantity: 2 });
+let salePrice = ref(0);
 
 effect(() => {
-  const total = product.price * product.quantity;
+  const total = salePrice.value * product.quantity;
   console.log(`total = ${total}`);
+});
+
+effect(() => {
+  salePrice.value = product.price * 0.9;
+  // console.log(`sale price = ${salePrice.value}`);
 });
 
 product.price = 10;
